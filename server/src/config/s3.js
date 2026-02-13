b807bff6d302afac345b308e23a1f0cda1e214d6
omar-elhadi <omar.el-hadi@laplateforme.io>
2026-02-13 17:44:58 +0100

    feat: Intégration d'AWS S3 pour le chargement et la récupération de fichiers
    > - Ajout des dépendances du SDK AWS pour les opérations S3 sur Scaleway
    
    > - Configuration du client S3 avec les identifiants et le point de terminaison Scaleway
    
    > - Implémentation de la fonctionnalité de chargement de fichiers à l'aide de multer et de la commande PutObjectCommand de Scaleway
    
    > - Création d'une route pour générer des URL signées permettant d'accéder aux fichiers chargés
    
    > - Ajout d'une page HTML de test pour le chargement de fichiers et la récupération des URL
    
    > - Mise à jour du point d'entrée du serveur pour inclure le point de terminaison de chargement.

--- server/src/config/s3.js
+++ server/src/config/s3.js
@@ -1,11 +1,11 @@
 import { S3Client } from "@aws-sdk/client-s3";
 
 export const s3 = new S3Client({
-  region: process.env.S3_REGION,
-  endpoint: process.env.S3_ENDPOINT,
+  region: process.env.SCALEWAY_REGION,
+  endpoint: process.env.SCALEWAY_ENDPOINT,
   credentials: {
-    accessKeyId: process.env.S3_ACCESS_KEY,
-    secretAccessKey: process.env.S3_SECRET_KEY,
+    accessKeyId: process.env.SCALEWAY_ACCESS_KEY,
+    secretAccessKey: process.env.SCALEWAY_SECRET_KEY,
   },
   forcePathStyle: true,
 });
