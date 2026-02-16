0170b5d512c947adec71fb9107bc1de73f59e78b
Roxane Sarpi <roxane.sarpi@laplateforme.io>
2026-02-16 16:00:05 +0100

    code pour la page MovieDetails

--- client/src/pages/MovieDetails/MovieDetails.jsx
+++ client/src/pages/MovieDetails/MovieDetails.jsx
@@ -1,18 +1,38 @@
 import React from 'react';
+import VideoPlayer from '../../components/VideoPlayer/VideoPlayer';
+import DirectorInfo from '../../components/DirectorInfo/DirectorInfo';
+import Synopsis from '../../components/Synopsis/Synopsis';
 
-const VideoPlayer = ({ youtubeId }) => {
+const MovieDetails = () => {
+  // Données temporaires pour l'exemple
+  const movieData = {
+    youtubeId: 'dQw4w9WgXcQ',
+    director: 'Nom du Réalisateur',
+    aiTools: ['ChatGPT', 'Midjourney', 'Runway'],
+    socialMedia: {
+      twitter: 'https://twitter.com/username',
+      instagram: 'https://instagram.com/username',
+      linkedin: 'https://linkedin.com/in/username'
+    },
+    synopsis: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+  };
+
   return (
-    <div className="flex-1 bg-black rounded-lg overflow-hidden">
-      <iframe
-        className="w-full h-full"
-        src={`https://www.youtube.com/embed/${youtubeId}`}
-        title="YouTube video player"
-        frameBorder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowFullScreen
-      ></iframe>
+    <div className="flex h-screen gap-5 p-5 bg-gray-900">
+      {/* Partie gauche - Lecteur vidéo */}
+      <VideoPlayer youtubeId={movieData.youtubeId} />
+
+      {/* Partie droite - Informations */}
+      <div className="flex-1 flex flex-col gap-5">
+        <DirectorInfo 
+          director={movieData.director}
+          aiTools={movieData.aiTools}
+          socialMedia={movieData.socialMedia}
+        />
+        <Synopsis synopsis={movieData.synopsis} />
+      </div>
     </div>
   );
 };
 
-export default VideoPlayer;
+export default MovieDetails;
