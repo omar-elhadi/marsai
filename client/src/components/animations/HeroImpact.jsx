26d662a305be4b0c3b65f2ebc34a74ef56587949
drajoan258 <charlestedison@gmail.com>
2026-02-21 11:23:45 +0100

    changement de sous-titre

--- client/src/components/animations/HeroImpact.jsx
+++ client/src/components/animations/HeroImpact.jsx
@@ -1103,7 +1103,7 @@ export default function HeroImpact() {
         className="relative z-40 mt-6 md:mt-12 opacity-0 pointer-events-none px-4"
       >
         <p className="text-[0.6rem] sm:text-sm md:text-xl text-zinc-400 font-light tracking-[0.25em] sm:tracking-[0.4em] md:tracking-[0.5em] uppercase text-center">
-          L&rsquo;apogée du cinéma génératif
+          Le nouveau commencement
         </p>
       </div>
     </div>
