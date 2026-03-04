89fdc369d39184b2f1d086c0c9e1bf5b10c287f1
omar-elhadi <omar.el-hadi@laplateforme.io>
2026-03-04 11:40:17 +0100

    fix(route): remove duplicated hard-coded routes form old code

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -151,22 +151,6 @@ function AppInner() {
 					<Route path={ROUTES.REGLES_CONDITIONS} element={<ReglesConditions />} />
 					<Route path={ROUTES.NEWS} element={<FestivalNews />} />
 					<Route path={ROUTES.EVENTS} element={<Events />} />
-					<Route path="/" element={<Home />} />
-					<Route path="/Newsletters" element={<Newsletters />} />
-					<Route path="/galerie" element={<Gallery />} />
-					<Route path="/soumettre" element={<SubmissionPage />} />
-					<Route path="/login" element={<LoginAdmin />} />
-					<Route path="/contact" element={<Contact />} />
-					<Route path="/Mention" element={<Mention />} />
-					<Route path="/cookies" element={<Cookies />} />
-					<Route path="/PolitiqueDeConfidentialite" element={<PolitiqueDeConfidentialite />} />
-					<Route path="/conditions-utilisations" element={<ConditionsUtilisations />} />
-					<Route path="/FAQ" element={<FAQ />} />
-					<Route path="/calendrier" element={<Calendrier />} />
-					<Route path="/login/verify" element={<VerifyToken />} />
-					<Route path="/regles-conditions" element={<ReglesConditions />} />
-					<Route path="/news" element={<FestivalNews />} />
-					<Route path="/events" element={<Events />} />
 					<Route path="/edit-film/:token" element={<EditFilmPage />} />
 					<Route path="/suivi" element={<TrackingPage />} />
 					<Route path="/palmares" element={<PalmaresPage />} />
