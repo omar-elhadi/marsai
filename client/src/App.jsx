fc0e43b888e608a4c2efd43135ebecb7962bfb36
Dalin <james.dalin@laplateforme.io>
2026-03-10 14:05:26 +0100

    fix: les pages palmarès et sélection est maintenant fixé

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -170,9 +170,10 @@ function AppInner() {
 				<Route element={<ProtectedRoute />}>
 					<Route path={ROUTES.ADMIN} element={<AdminLayout />}>
 						<Route index element={<DashboardHome />} />
-						<Route path={ROUTES.ADMIN_FILMS} element={<FilmsList />} />
-						<Route path={ROUTES.ADMIN_USERS} element={<AdminDashboard />} />
-						<Route path={ROUTES.ADMIN_AWARDS} element={<PalmaresPage />} />
+						<Route path={ROUTES.ADMIN_FILMS}      element={<FilmsList />} />
+						<Route path={ROUTES.ADMIN_USERS}      element={<AdminDashboard />} />
+						<Route path={ROUTES.ADMIN_SELECTION}  element={<SelectionPage />} />
+						<Route path={ROUTES.ADMIN_AWARDS}     element={<AwardsPage />} />
 					</Route>
 				</Route>
 
