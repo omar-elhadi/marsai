82e6a9ff4b36feaee4304795611dbd047a5f69ff
Kaneki-Ken993 <omerelhadi222@gmail.com>
2026-05-26 09:13:48 +0200

    Fix typescript errors in CategoryHeader

--- client/src/pages/Events/components/CategoryHeader.tsx
+++ client/src/pages/Events/components/CategoryHeader.tsx
@@ -6,7 +6,7 @@
 
 const GRAIN = `url("data:image/svg+xml,%3Csvg viewBox='0 0 256 256' xmlns='http://www.w3.org/2000/svg'%3E%3Cfilter id='n'%3E%3CfeTurbulence type='fractalNoise' baseFrequency='0.88' numOctaves='4' stitchTiles='stitch'/%3E%3C/filter%3E%3Crect width='100%25' height='100%25' filter='url(%23n)'/%3E%3C/svg%3E")`;
 
-export default function CategoryHeader({ title, subtitle, image, date }) {
+export default function CategoryHeader({ title, subtitle, image, date }: { title: string, subtitle: string, image: string, date?: string }) {
   return (
     <>
       <div style={{
